9a1cb5b7a7bdb9262e487f8ac7410e70d1c85825
sulimanalhassan123as-code <sulimanalhassan.123.as@gmail.com>
2025-10-13 18:00:05 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,20 @@
-const CACHE_NAME = "suleiman-calculator-v1";
-const urlsToCache = [
-  "index.html",
-  "manifest.json",
-  "icon-192.png",
-  "icon-512.png"
+const CACHE_NAME = "calc-app-v1";
+const ASSETS = [
+  "/Never-/",
+  "/Never-/index.html",
+  "/Never-/manifest.json",
+  "/Never-/icon-192.png",
+  "/Never-/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
